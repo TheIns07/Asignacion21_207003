e183d1ccc06f9f81d676251ddf3307a7724ac305
Arturo Cervantes <jesus.cervantes207003@potros.itson.edu.mx>
2022-11-29 22:40:54 -0700

    Encuesta

--- encuesta2.py
+++ encuesta2.py
@@ -3,92 +3,93 @@ class Encuesta:
         print('¿Cuantos alumnos desea registrar?')
         cantidadAlumnos = int(input())
         i = 0
-        diccionario = dict()
+        my_dict = {"Sexo": [], "ID": [], "Carrera": []}
         while i < cantidadAlumnos:
-
             print('Encuesta a alumnos')
             ide = input('Matricula:')
             sex = input('Sexo (M o F): ')
-            carrer = input('Carrera: (‘IE’, ‘ISW’, ‘IC’, LAE’, ‘LPS’, etc.))')
-            diccionario.fromkeys('sexo', [sex])
-            diccionario.fromkeys('id', [ide])
-            diccionario.fromkeys('carrera', [carrer])
-            diccionario.fromkeys('carrera', [carrer])
+            carrer = input('Carrera: (‘IE’, ‘ISW’, ‘IC’, LAE’, ‘LPS’, etc.): ')
+            my_dict["Sexo"].append(sex)
+            my_dict["Carrera"].append(carrer)
+            my_dict["ID"].append(ide)
             i = i+1
-        return diccionario
+        return my_dict
 
-    def despEncuesta(dicc):
+    def clasificaAlumnosCategoria(carrera, sexo):
+        print("******************")
+        print("ie, isw, ic, lae, otros")
+        print(carrera)
+        print("Mujeres, Hombres")
+        print("******************")
+        print(sexo)
+        print("******************")
+        return alumnos
+
+    def despEncuesta(listaAlumnos):
         print('***************')
-        print('      ID      |      Sexo      |     Carrera')
-        print(dicc.items())
+        print("ID :", listaAlumnos['ID'])
+        print("Carrera :", listaAlumnos['Carrera'])
+        print("Sexo :", listaAlumnos['Sexo'])
 
-    def clasificaAlumnosCategoria(dicc):
+    def clasificaAlumnosCarrera(dicc):
         # se instancia cel como arreglo
-        alumno = list(dicc.values())
         # se define el tamaño de la lista
-        i = 0
-        hombre = 0
-        mujer = 0
         ie = 0
         isw = 0
         ic = 0
         lae = 0
         otros = 0
 
-        for i in alumno:
-            if alumno.get(i) == 'M':
-                hombre = hombre+1
-            elif alumno.get(i) == 'F':
-                mujer = mujer+1
+        for val in dicc.values():
+            if val.__eq__("IE".upper().lower()):
+                ie = ie + 1
+            elif val.__eq__("ISW".upper().lower()):
+                isw = isw+1
+            elif val.__eq__("IC".upper().lower()):
+                ic = ic+1
+            elif val.__eq__("LAE".upper().lower()):
+                lae = lae + 1
             else:
-                if alumno.get(i) == 'IE':
-                    ie = ie+1
-                elif alumno.get(i) == 'ISW':
-                    isw = isw+1
-                elif alumno.get(i) == 'IC':
-                    ic = ic+1
-                elif alumno.get(i) == 'LAE':
-                    lae = lae+1
-                else:
-                    otros = otros+1
+                otros += 1
 
-        print('Hombres:', hombre)
-        print('Mujeres:', mujer)
-        print('IE:', ie)
-        print('ISW:', isw)
-        print('IC:', ic)
-        print('LAE:', lae)
-        print('Otros:', otros)
+        listaDatos = [ie, isw, ic, lae, otros]
+        return listaDatos
 
-    def despCatAlumno(dicc):
-        i = 0
-        ide = input('Matricula: ID')
-        print(dicc[ide])
+    def despAlumnosCarrera(listaDatos):
+        print('IE:', listaDatos[0])
+        print('ISW:', listaDatos[1])
+        print('IC:', listaDatos[2])
+        print('LAE:', listaDatos[3])
+        print('Otros:', listaDatos[4])
 
-    def clasificaAlumnosCategoria(alumnos):
-        # se instancia cel como arreglo
-        alumno = alumnos
-        # se define el tamaño de la lista
-        tamañoLista = len(alumno)
-        i = 0
-        calificaciones = {}
-        for i in alumno:
-            ide = input('Calificar alumno: ')
-            dicc = dict.fromkeys(llaves, 5)
+    def clasificaAlumnosSexo(dicc):
+        hombre = 0
+        mujer = 0
+        for val in dicc.values():
+            if val.__eq__('M'.upper().lower()):
+                hombre = hombre+1
+            elif val.__eq__('F'.upper().lower()):
+                mujer = mujer+1
+        listaDatos = [hombre, mujer]
+        return listaDatos
 
-        return calificaciones
+    def despAlumnosSexo(lista):
+        print('Mujeres:', lista[0])
+        print('Hombres:', lista[1])
 
 
 clase = Encuesta
 alumnos = clase.leeEncuesta()
+sexo = clase.clasificaAlumnosSexo(alumnos)
+carrera = clase.clasificaAlumnosCarrera(alumnos)
 
 while True:
     print('********************************')
     print('Seleccione lo que desee hacer: \n')
     print('1) Desplegar Encuesta\n')
-    print('2) Imprimir alumnos por categoria\n')
-    print('3) Filtrar Alumnos\n')
-    print('4) Calificar Alumnos\n')
+    print('2) Imprimir alumnos en general\n')
+    print('3) Imprimir alumnos por sexo\n')
+    print('4) Imprimir alumnos por carrera\n')
     print('5) Salir\n')
     print('********************************')
 
@@ -98,10 +99,10 @@ while True:
     if comando == 1:
         clase.despEncuesta(alumnos)
     if comando == 2:
-        clase.clasificaAlumnosCategoria(alumnos)
+        clase.clasificaAlumnosCategoria(carrera, sexo)
     if comando == 3:
-        clase.despCatAlumno(alumnos)
+        clase.despAlumnosSexo(sexo)
     if comando == 4:
-        clase.clasificaAlumnosCategoria(alumnos)
+        clase.despAlumnosCarrera(carrera)
     if comando == 5:
         exit()
